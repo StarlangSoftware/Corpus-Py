130fd2d3192851263b13fa0150b57ab82d42d20f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:16:19 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-Corpus',
     version='1.0.11',
     packages=['Corpus'],
-    url='https://github.com/olcaytaner/Corpus-Py',
+    url='https://github.com/StarlangSoftware/Corpus-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Corpus library',
     install_requires=['NlpToolkit-Dictionary']
 )
