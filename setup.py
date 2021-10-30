69575022cfa361ff72add29b33df2fbb95abbd91
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:47:46 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-Corpus',
-    version='1.0.12',
+    version='1.0.13',
     packages=['Corpus'],
     url='https://github.com/StarlangSoftware/Corpus-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Corpus library',
-    install_requires=['NlpToolkit-Dictionary']
+    install_requires=['NlpToolkit-Dictionary'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
